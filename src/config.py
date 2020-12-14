84f4a13878fd9aff14e5c9bf4431b3d7466d170f
marcospiau <marcospiau123@gmail.com>
2020-12-14 04:37:36 +0000

    Change config.py

--- src/config.py
+++ src/config.py
@@ -1,15 +1,7 @@
 import yaml
 
 
-# main config file, should be in root path
-MAIN_CONFIG_FILE="./config.yml"
-
 def parse_yaml_file(x):
     with open(x, 'r') as f:
         parsed_yaml_file = yaml.load(f, Loader=yaml.FullLoader)
     return parsed_yaml_file
-
-def load_main_config():
-    return parse_yaml_file(MAIN_CONFIG_FILE)
-
-
