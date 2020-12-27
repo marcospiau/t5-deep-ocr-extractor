5347989ef92ea4dd87fd9d92d75e6530c06604c9
marcospiau <marcospiau123@gmail.com>
2020-12-27 16:22:07 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='final_project_ia376j',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Final Projetct IA376J - SROIE Task 3`',
+    author='marcospiau',
+    license='MIT',
+)
