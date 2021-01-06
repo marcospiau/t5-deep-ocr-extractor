d0a12e1c60f7ce7186ef8162f336b06c64068910
marcospiau <marcospiau123@gmail.com>
2021-01-06 01:10:48 -0300

    Added function to extract OCR vertices

--- src/data/google_vision_ocr_parsing.py
+++ src/data/google_vision_ocr_parsing.py
@@ -2,7 +2,8 @@ from typing import Dict
 
 
 def extract_full_text_annotation(x: Dict[str, str]) -> str:
-    """Extract full text annotation from a single OCR output.
+    """Extract full text annotation from a single OCR output from Google Cloud
+    Vision API
 
     Args:
         x (Dict[str, str]): whole OCR output.
@@ -12,3 +13,27 @@ def extract_full_text_annotation(x: Dict[str, str]) -> str:
     """
     raw_full_text = x["fullTextAnnotation"]["text"]
     return raw_full_text
+
+
+def extract_text_and_vertices(x: Dict[str, str]):
+    """Extracts all annotations and bounding box vertices from a single OCR
+        output from Google Cloud Vision API.
+
+    The first element is the full OCR. It's equivalent to the output of
+    `extract_full_text_annotation` for the same OCR output.
+
+    Args:
+        x (Dict[str, str]): whole OCR output.
+
+    Returns:
+        list where each item is a tuple where the first element is the text and
+        the second are the 4 vertices of the corresponding bounding box.
+    """
+    blocks = []
+    for annotation in x["textAnnotations"]:
+        text = annotation['description']
+        vertices = [
+            tuple(x.values()) for x in annotation['boundingPoly']['vertices']
+        ]
+        blocks.append((text, vertices))
+    return blocks
