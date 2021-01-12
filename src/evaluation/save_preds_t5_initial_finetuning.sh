0158a44f9c5b9dca1674eccbc3d4d150d17bf3cf
marcospiau <marcospiau123@gmail.com>
2021-01-12 01:44:42 +0000

    Added script used for saving predictions for all models in initial finetune

---
+++ src/evaluation/save_preds_t5_initial_finetuning.sh
@@ -0,0 +1,9 @@
+predictions_basedir=/home/marcospiau/final_project_ia376j/data/sroie_receipt_dataset/predictions
+for i in 27 28 29 30 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48
+do
+    exp_id="$exp_ids FIN-$i"
+    python3 save_experiment_predictions_t5_ocr_baseline.py \
+        --neptune_experiment_id="FIN-$i" \
+        --input_path="/home/marcospiau/final_project_ia376j/data/sroie_receipt_dataset/$1/" \
+        --output_path="$predictions_basedir/FIN-$i/$1"
+done
