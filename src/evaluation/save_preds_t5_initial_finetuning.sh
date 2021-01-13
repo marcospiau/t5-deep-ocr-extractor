b880acf936efdfdc404b4a0b2eb2fb76c53ef15d
marcospiau <marcospiau123@gmail.com>
2021-01-13 06:34:58 +0000

    Final submissions made on competition site

--- src/evaluation/save_preds_t5_initial_finetuning.sh
+++ src/evaluation/save_preds_t5_initial_finetuning.sh
@@ -1,7 +1,7 @@
+# The argument on the input is the partition ('train' or 'test')
 predictions_basedir=/home/marcospiau/final_project_ia376j/data/sroie_receipt_dataset/predictions
 for i in 27 28 29 30 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48
 do
-    exp_id="$exp_ids FIN-$i"
     python3 save_experiment_predictions_t5_ocr_baseline.py \
         --neptune_experiment_id="FIN-$i" \
         --input_path="/home/marcospiau/final_project_ia376j/data/sroie_receipt_dataset/$1/" \
